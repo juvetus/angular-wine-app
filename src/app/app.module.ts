1deeddf051b48a200172fe2cf0093ff3f6ae6000
ashokkavungal5 <ashokkavungal5@gmail.com>
2020-05-06 12:42:44 +0200

    created components header ,recipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipeDetailsComponent } from './recipe/recipe-details/recipe-details.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipeDetailsComponent
   ],
   imports: [
     BrowserModule
