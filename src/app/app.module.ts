0a89245dce0903667adf3e31f984e90c8ed63c52
ashokkavungal5 <ashokkavungal5@gmail.com>
2020-05-06 12:49:38 +0200

    create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { RecipeDetailsComponent } from './recipe/recipe-details/recipe-details.component';
-import { RecipeItemComponent } from './recipe/recipe-list/recipe-item/recipe-item.component'
+import { RecipeItemComponent } from './recipe/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     RecipeDetailsComponent,
-    RecipeItemComponent
+    RecipeItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule
