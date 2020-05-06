a193b13cbd4ff4acd67ea2c040650cb43eec6ad5
ashokkavungal5 <ashokkavungal5@gmail.com>
2020-05-06 12:46:21 +0200

    refactored

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { RecipeDetailsComponent } from './recipe/recipe-details/recipe-details.component'
+import { RecipeDetailsComponent } from './recipe/recipe-details/recipe-details.component';
+import { RecipeItemComponent } from './recipe/recipe-list/recipe-item/recipe-item.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    RecipeDetailsComponent
+    RecipeDetailsComponent,
+    RecipeItemComponent
   ],
   imports: [
     BrowserModule
