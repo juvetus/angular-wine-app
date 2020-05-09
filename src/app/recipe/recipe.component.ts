15cfedeba00d6cf06a61e8bb4e94f99ce5da8528
ashokkavungal5 <ashokkavungal5@gmail.com>
2020-05-09 13:56:22 +0200

    refactored - selected recipe using service

--- src/app/recipe/recipe.component.ts
+++ src/app/recipe/recipe.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {Recipe} from './recipe.model';
 import { RecipeService } from './recipe.service';
 
@@ -9,7 +9,18 @@ import { RecipeService } from './recipe.service';
   styleUrls: ['./recipe.component.css'],
   providers: [RecipeService] //availing service to all childs of recipe comp
 })
-export class RecipeComponent {
+export class RecipeComponent implements OnInit{
   selectedRecipe: Recipe;
 
+  constructor(private recipeService: RecipeService) { } //to make sure the same instance is used
+
+  ngOnInit() {
+    this.recipeService.recipeSelected
+      .subscribe(
+        (recipe: Recipe) => {
+          /* updates accordingly to recipe select event */
+          this.selectedRecipe = recipe;
+        }
+      );
+  }
 }
--- src/app/recipe/recipe.service.ts
+++ src/app/recipe/recipe.service.ts
@@ -1,7 +1,10 @@
 import { Recipe } from './recipe.model';
+import { EventEmitter } from '@angular/core';
 
 
 export class RecipeService {
+
+    recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
         new Recipe('chicken biriyani', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
