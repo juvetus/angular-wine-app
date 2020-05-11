156129b02ffd89491521c8d9e8d95a5d8498c8f2
ashokkavungal5 <ashokkavungal5@gmail.com>
2020-05-11 20:16:37 +0200

    edit allowed only if recipe id included

--- src/app/recipe/recipe-edit/recipe-edit.component.ts
+++ src/app/recipe/recipe-edit/recipe-edit.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-edit.component.css']
 })
 export class RecipeEditComponent implements OnInit {
+  editmode : boolean = false;
+  recipe_id : number ;
 
-  constructor() { }
+  constructor(private route : ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.params.subscribe(
+      (params:Params) =>{
+        this.recipe_id = +params['id'];
+        this.editmode = params['id'] != null; //turns edit mode t only when id is valid number
+        console.log(this.editmode);
+      }
+    )
   }
 
 }
