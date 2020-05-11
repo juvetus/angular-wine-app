b7f1825e654a24439ed677eef57fbd3e114dbc01
ashokkavungal5 <ashokkavungal5@gmail.com>
2020-05-11 16:43:55 +0200

    header nav - routing

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,16 +1,9 @@
-import { Component, EventEmitter,Output } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
     selector:'app-header',
     templateUrl:'./header.component.html'
 })
 export class HeaderComponent{
-
-
-    @Output() selectedComponent = new EventEmitter<string>(); //an event to which app can listen
-
-    onSelect(selectedComp:string){
-        this.selectedComponent.emit(selectedComp)
-    }
 
 }
