5693d577b5cd3f3fdc1b629ee65d1b0d31f6460e
ashokkavungal5 <ashokkavungal5@gmail.com>
2020-05-15 19:49:37 +0200

    make sure fetched data contain ingrediens key using pip-map

--- src/app/shared/firebase.service.ts
+++ src/app/shared/firebase.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Recipe } from '../recipe/recipe.model';
 import { RecipeService } from '../recipe/recipe.service';
+import { map } from 'rxjs/operators';
+
+
 
 @Injectable({providedIn:"root"}) //another way of adding any service to provider
 export class BackendService{
@@ -22,6 +25,14 @@ export class BackendService{
     fetchRecipe(){
         this.http.get<Recipe[]>(
             'https://recipeapp-c6b15.firebaseio.com/recipes.json'
+            ).pipe(map(recipes =>{
+                return recipes.map(recipe =>{
+                    return {
+                        ...recipe,
+                        ingredients: recipe.ingredients ? recipe.ingredients :[]
+                    }
+                });
+            })    
             ).subscribe(
             res=>{
                 this.recipeService.setFetchedRecipes(res);
