71b103b2d246cee538821ad9cc1c1c78b23a5496
ashokkavungal5 <ashokkavungal5@gmail.com>
2020-05-16 15:45:44 +0200

    sign up to fire base , gen token

--- src/app/recipe/auth/auth.component.ts
+++ src/app/recipe/auth/auth.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { AuthService} from './../auth/auth.service'
 
 @Component({
     selector: 'app-auth',
@@ -8,12 +9,28 @@ import { NgForm } from '@angular/forms';
 export class AuthComponent{
     logInMode = false;
 
+    constructor(private authService : AuthService){}
+
     switchAuthMode(){
         this.logInMode = !this.logInMode;
     }
 
     onAuthFormSubmit(authForm : NgForm){
-        console.log(authForm.value);
+
+        if (authForm.invalid){ return ;}
+
+
+        const email = authForm.value.email;
+        const password = authForm.value.password;
+        if(this.logInMode){
+
+        }else{
+            this.authService.signUp(email,password).subscribe(res=>{
+                console.log(res);
+            },err=>{
+                console.log(err);
+            });
+        }
         authForm.reset();
     }
 }
