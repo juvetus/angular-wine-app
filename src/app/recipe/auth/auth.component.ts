6d69d27499679b4683326576d9495b1a43d6acab
ashokkavungal5 <ashokkavungal5@gmail.com>
2020-05-16 14:35:15 +0200

    form auth input handle

--- src/app/recipe/auth/auth.component.ts
+++ src/app/recipe/auth/auth.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
     selector: 'app-auth',
@@ -9,5 +10,10 @@ export class AuthComponent{
 
     switchAuthMode(){
         this.logInMode = !this.logInMode;
+    }
+
+    onAuthFormSubmit(authForm : NgForm){
+        console.log(authForm.value);
+        authForm.reset();
     }
 }
