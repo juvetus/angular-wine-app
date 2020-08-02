f50723bfffc0595dee1f5d83b4447d8268b7518d
ashokkavungal5@gmail.com <ashokkavungal5@gmail.com>
2020-08-03 01:54:53 +0200

    added side effect -sign in

--- src/app/recipe/auth/auth.component.ts
+++ src/app/recipe/auth/auth.component.ts
@@ -2,10 +2,15 @@ import { Component, ComponentFactoryResolver, ViewChild, OnDestroy } from '@angu
 import { NgForm } from '@angular/forms';
 import { AuthService, AuthResponse} from './../auth/auth.service'
 import { Observable, Subscription } from 'rxjs';
+import { Store } from '@ngrx/store';
+
 import { Router } from '@angular/router';
 import { AlertComponent } from 'src/app/shared/alert/alert.component';
 import { PlaceHolderDirective } from '../../shared/placeholder/placeholder.directive';
 import { BackendService } from '../../shared/firebase.service';
+import * as fromApp from '../../store/app.reducer';
+import * as AuthActions from './store/auth.actions';
+
 
 
 @Component({
@@ -25,7 +30,18 @@ export class AuthComponent implements OnDestroy{
         private router: Router,
         private compFacRes : ComponentFactoryResolver,
         private backendService : BackendService,
+        private store: Store<fromApp.AppState>
         ){}
+
+        ngOnInit() {
+            this.store.select('auth').subscribe(authState => {
+              this.isLoading = authState.loading;
+              this.error = authState.authError;
+              if (this.error) {
+                this.showAlertDynam(this.error);
+              }
+            });
+          }    
 
     switchAuthMode(){
         this.logInMode = !this.logInMode;
@@ -40,7 +56,7 @@ export class AuthComponent implements OnDestroy{
         }
     }
 
-    private showAlertDynam(message: string){
+    private showAlertDynam(message: String){
              
         const alertCompFactory = this.compFacRes.resolveComponentFactory(AlertComponent);
         const alertHost = this.alertHolder.viewContainerRef;
@@ -72,12 +88,15 @@ export class AuthComponent implements OnDestroy{
         let authObservable : Observable<AuthResponse>; 
 
         if(this.logInMode){
-            authObservable = this.authService.signIn(email,password);
+            //authObservable = this.authService.signIn(email,password);
+            this.store.dispatch(
+                new AuthActions.LoginStart({ email: email, password: password })
+              );
         }else{
             authObservable= this.authService.signUp(email,password);
         }
 
-        authObservable.subscribe(res=>{
+     /*    authObservable.subscribe(res=>{
             this.isLoading = false;
             this.router.navigate(['/recipes']);
             this.autoLoadList();
@@ -85,7 +104,7 @@ export class AuthComponent implements OnDestroy{
             this.isLoading = false;
             this.error = defErrorMsg;
             this.showAlertDynam(defErrorMsg);
-        });
+        }); */
 
         authForm.reset();
     }
